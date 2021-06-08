b8a8217f4f2066389a65e95cda39f1cdf52bf98d
PC1 <mohamednagy2015@outlook.com>
2021-06-08 12:39:22 +0400

    Extractor: Mask creation done

--- vehicle-segmentation/image_processing.py
+++ vehicle-segmentation/image_processing.py
@@ -4,23 +4,98 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 from parameters import *
+from tqdm import tqdm
 
-# Identify number of classes
-
-def vehicleExtractor(source, dist):
+def sidewalkExtractor(source, dist):
 	list_imgs = os.listdir(source)
-	
+
 	os.mkdir(dist)
-	
-	for img_name in list_imgs:
+	looper = tqdm(list_imgs, leave=False)
+	for img_name in looper:
 		out = cv2.cvtColor(cv2.imread(source + img_name), cv2.COLOR_BGR2RGB)
-		# Extract vehicles
+
+		# Extract sidewalk
 		mask = np.zeros((out.shape[0], out.shape[1]))
-		mask[out[:,:,2] == 142 ] = 255
-		mask[out[:,:,2] == 230 ] = 255
+		mask[np.sum(out == np.array(SIDEWALK_COLOR), 2) == 3] = 255.0
+
 		# Write
 		cv2.imwrite(dist + img_name, mask)
-	
 
-vehicleExtractor(source = TRAIN_MASK_IMG_DIR, dist= TRAIN_MASK_VEHICLE_DIR)
-vehicleExtractor(source = VAL_MASK_IMG_DIR, dist = VAL_MASK_VEHICLE_DIR)
+
+def boundaryExtractor(source, dist):
+	list_imgs = os.listdir(source)
+
+	os.mkdir(dist)
+	looper = tqdm(list_imgs, leave=False)
+	for img_name in looper:
+		out = cv2.cvtColor(cv2.imread(source + img_name), cv2.COLOR_BGR2RGB)
+
+		# Extract sidewalk
+		mask = np.zeros((out.shape[0], out.shape[1]))
+		mask[np.sum(out == np.array(BOUNDARY_COLOR), 2) == 3] = 255.0
+
+		# Write
+		cv2.imwrite(dist + img_name, mask)
+
+
+def roadExtractor(source, dist):
+	list_imgs = os.listdir(source)
+
+	os.mkdir(dist)
+	looper = tqdm(list_imgs, leave=False)
+	for img_name in looper:
+		out = cv2.cvtColor(cv2.imread(source + img_name), cv2.COLOR_BGR2RGB)
+
+		# Extract sidewalk
+		mask = np.zeros((out.shape[0], out.shape[1]))
+		mask[np.sum(out == np.array(ROAD_COLOR), 2) == 3] = 255.0
+
+		# Write
+		cv2.imwrite(dist + img_name, mask)
+
+
+def lanelineExtractor(source, dist):
+	list_imgs = os.listdir(source)
+
+	os.mkdir(dist)
+	looper = tqdm(list_imgs, leave=False)
+	for img_name in looper:
+		out = cv2.cvtColor(cv2.imread(source + img_name), cv2.COLOR_BGR2RGB)
+
+		# Extract sidewalk
+		mask = np.zeros((out.shape[0], out.shape[1]))
+		mask[np.sum(out == np.array(LANELINE_COLOR), 2) == 3] = 255.0
+
+		# Write
+		cv2.imwrite(dist + img_name, mask)
+
+
+def pointlineExtractor(source, dist):
+	list_imgs = os.listdir(source)
+
+	os.mkdir(dist)
+	looper = tqdm(list_imgs, leave=False)
+	for img_name in looper:
+		out = cv2.cvtColor(cv2.imread(source + img_name), cv2.COLOR_BGR2RGB)
+
+		# Extract sidewalk
+		mask = np.zeros((out.shape[0], out.shape[1]))
+		mask[np.sum(out == np.array(POINTSLINE_COLOR), 2) == 3] = 255.0
+
+		# Write
+		cv2.imwrite(dist + img_name, mask)
+
+sidewalkExtractor(source= TRAIN_MASK_IMG_DIR, dist = TRAIN_MASK_SIDEWALK_DIR)
+sidewalkExtractor(source= VAL_MASK_IMG_DIR, dist = VAL_MASK_SIDEWALK_DIR)
+
+lanelineExtractor(source= TRAIN_MASK_IMG_DIR, dist = TRAIN_MASK_LANELINE_DIR)
+lanelineExtractor(source= VAL_MASK_IMG_DIR, dist = VAL_MASK_LANELINE_DIR)
+
+roadExtractor(source= TRAIN_MASK_IMG_DIR, dist = TRAIN_MASK_ROAD_DIR)
+roadExtractor(source= VAL_MASK_IMG_DIR, dist = VAL_MASK_ROAD_DIR)
+
+pointlineExtractor(source= TRAIN_MASK_IMG_DIR, dist = TRAIN_MASK_LANEPOINT_DIR)
+pointlineExtractor(source= VAL_MASK_IMG_DIR, dist = VAL_MASK_LANEPOINT_DIR)
+
+boundaryExtractor(source= TRAIN_MASK_IMG_DIR, dist = TRAIN_MASK_BOUNDARY_DIR)
+boundaryExtractor(source= VAL_MASK_IMG_DIR, dist = VAL_MASK_BOUNDARY_DIR)
--- vehicle-segmentation/parameters.py
+++ vehicle-segmentation/parameters.py
@@ -1,7 +1,6 @@
 import torch 
 import os
 
-
 # Model Parameters
 LEARNING_RATE = 0.00001
 DEVICE        = "cuda" if torch.cuda.is_available() else "cpu"
@@ -11,12 +10,24 @@ NUM_WORKERS   = 2
 IMAGE_SIZE    = 320
 
 # Path Parameters
-TRAIN_IMG_DIR      		= os.path.dirname(os.getcwd()) + "/data/training/images/"
-TRAIN_MASK_IMG_DIR 		= os.path.dirname(os.getcwd()) + "/data/training/v2.0/labels/"
-TRAIN_MASK_VEHICLE_DIR 	= os.path.dirname(os.getcwd()) + "/data/training/v2.0/labels_vehicle/"
-VAL_IMG_DIR        		= os.path.dirname(os.getcwd()) + "/data/validation/images/"
-VAL_MASK_IMG_DIR 		= os.path.dirname(os.getcwd()) + "/data/validation/v2.0/labels/"
-VAL_MASK_VEHICLE_DIR    = os.path.dirname(os.getcwd()) + "/data/validation/v2.0/labels_vehicle/"
+TRAIN_IMG_DIR       = os.path.dirname(os.getcwd()) + "/data/training/images/"
+TRAIN_MASK_IMG_DIR 	= os.path.dirname(os.getcwd()) + "/data/training/v2.0/labels/"
+
+VAL_IMG_DIR        	= os.path.dirname(os.getcwd()) + "/data/validation/images/"
+VAL_MASK_IMG_DIR 	= os.path.dirname(os.getcwd()) + "/data/validation/v2.0/labels/"
+
+TRAIN_MASK_SIDEWALK_DIR  = os.path.dirname(os.getcwd()) + "/data/training/v2.0/labels_sidewalk/"
+TRAIN_MASK_ROAD_DIR  	 = os.path.dirname(os.getcwd()) + "/data/training/v2.0/labels_road/"
+TRAIN_MASK_LANELINE_DIR  = os.path.dirname(os.getcwd()) + "/data/training/v2.0/labels_laneline/"
+TRAIN_MASK_LANEPOINT_DIR = os.path.dirname(os.getcwd()) + "/data/training/v2.0/labels_lanepoint/"
+TRAIN_MASK_BOUNDARY_DIR  = os.path.dirname(os.getcwd()) + "/data/training/v2.0/labels_boundary/"
+
+VAL_MASK_SIDEWALK_DIR  = os.path.dirname(os.getcwd()) + "/data/validation/v2.0/labels_sidewalk/"
+VAL_MASK_ROAD_DIR  	 = os.path.dirname(os.getcwd()) + "/data/validation/v2.0/labels_road/"
+VAL_MASK_LANELINE_DIR  = os.path.dirname(os.getcwd()) + "/data/validation/v2.0/labels_laneline/"
+VAL_MASK_LANEPOINT_DIR = os.path.dirname(os.getcwd()) + "/data/validation/v2.0/labels_lanepoint/"
+VAL_MASK_BOUNDARY_DIR  = os.path.dirname(os.getcwd()) + "/data/validation/v2.0/labels_boundary/"
+
 VIDEO_DIR 				= "input videos/"
 
 # Save Path
@@ -35,3 +46,10 @@ OUT_CHANNELS_GRAY = 1
 
 UNET_MODEL = "unet_vehicle"
 UNET_MODEL_PRETRAINED = "unet_vehicle_pretrained"
+
+# Colors
+SIDEWALK_COLOR   = [244, 35, 232]
+LANELINE_COLOR   = [255, 255, 255]
+POINTSLINE_COLOR = [128, 128, 128]
+BOUNDARY_COLOR   = [196, 196, 196]
+ROAD_COLOR       = [128, 64, 128]
